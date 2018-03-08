0b3b9d159dee46bb4a27406f61457f8343964dcc
unknown <pinalpbhiani123@gmail.com>
2018-03-08 19:39:19 +0530

    Edit java Class

---
+++ java/Emp.java
@@ -0,0 +1,37 @@
+public class Emp {
+    
+   private int Id;  
+private String Name,Password,Email,Country;  
+public int getId() {  
+    return Id;  
+}  
+public void setId(int id) {  
+    this.Id = id;  
+}  
+public String getName() {  
+    return Name;  
+}  
+public void setName(String name) {  
+    this.Name = name;  
+}  
+public String getPassword() {  
+    return Password;  
+}  
+public void setPassword(String password) {  
+    this.Password = password;  
+}  
+public String getEmail() {  
+    return Email;  
+}  
+public void setEmail(String email) {  
+    this.Email = email;  
+}  
+public String getCountry() {  
+    return Country;  
+}  
+public void setCountry(String country) {  
+    this.Country = country;  
+}  
+  
+
+}
